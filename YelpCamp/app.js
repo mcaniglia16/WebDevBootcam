ca411a04b011a70762051ebad27d4f957a4ea336
mcaniglia16 <50206147+mcaniglia16@users.noreply.github.com>
2021-03-20 23:37:35 -0400

    refactored routes

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -6,11 +6,8 @@ const Campground = require('./models/campground');
 const Review = require('./models/review');
 const methodOverride = require('method-override');
 const catchAsync = require('./utils/catchAsync');
-const expressError = require('./utils/ExpressError');
 const ExpressError = require('./utils/ExpressError');
-const {campgroundSchema} = require('./schemas.js');
-const {reviewSchema} = require('./schemas.js');
-const { findByIdAndUpdate } = require('./models/campground');
+const {campgroundSchema, reviewSchema} = require('./schemas.js');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -31,100 +28,22 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static('public'))
 
-///////////////////JOI SERVER SIDE DATA VALIDATION MIDDLEWARE
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
+//ROUTES
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
+app.use('/campgrounds', campgrounds);           //prefix all routes in campgrounds.js with /campgrounds
+app.use('/campgrounds/:id/reviews', reviews);   //prefix all routes in reviews.js with /reviews
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
 /////////////////////
-
 app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
-})
-
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard' });
-    await camp.save();
-    res.send(camp);
-})
-
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-})
-
-//                          OUR VALIDATION MIDDLEWARE
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
-    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`campgrounds/${campground._id}`);
-}))
-
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate("reviews");
-    console.log(campground);
-    res.render('campgrounds/show', { campground });
-}))
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', { campground });
-}))
-
-//                          OUR VALIDATION MIDDLEWARE
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds')
-}))
-
-//                                  OUR VALIDATION MIDDLEWARE
-app.post("/campgrounds/:id/reviews", validateReview, catchAsync(async(req, res) => {
-    const campground = await (await Campground.findById(req.params.id));
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-app.delete("/campgrounds/:id/reviews/:reviewId", catchAsync(async(req, res) => {
-    // const {id, reviewId}
-    await Campground.findByIdAndUpdate(req.params.id, {$pull: {reviews: req.params.reviewId}});
-    await Review.findByIdAndDelete(req.params.reviewID);
-    res.redirect(`/campgrounds/${req.params.id}`)
-}))
-
 //Order is very important: this will only run if no other route matched first
 app.all('*', (req, res, next) => {
-    next(new expressError('Page Not Found', 404));
+    next(new ExpressError('Page Not Found', 404));
 })
 
 app.use((err, req, res, next) => {
---
+++ YelpCamp/routes/reviews.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router({mergeParams: true});
+const catchAsync = require('./../utils/catchAsync');
+const ExpressError = require('./../utils/ExpressError');
+const Review = require('./../models/review');
+const Campground = require('./../models/campground');
+const {reviewSchema} = require('./../schemas.js');
+
+///////////////////JOI SERVER SIDE DATA VALIDATION MIDDLEWARE
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+//                                  OUR VALIDATION MIDDLEWARE
+router.post("/", validateReview, catchAsync(async(req, res) => {
+    const campground = await (await Campground.findById(req.params.id));
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+router.delete("/:reviewId", catchAsync(async(req, res) => {
+    // const {id, reviewId}
+    await Campground.findByIdAndUpdate(req.params.id, {$pull: {reviews: req.params.reviewId}});
+    await Review.findByIdAndDelete(req.params.reviewID);
+    res.redirect(`/campgrounds/${req.params.id}`)
+}))
+
+module.exports = router;
