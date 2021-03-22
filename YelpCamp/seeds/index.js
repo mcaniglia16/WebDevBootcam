925d85d3a2bc0ca1e0ee99d05c9ff5162cbc0317
mcaniglia16 <50206147+mcaniglia16@users.noreply.github.com>
2021-03-21 23:17:41 -0400

    started authorization

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 50);
         const camp = new Campground({
+            author: "6057ec0233f37e3710196cf3",
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
